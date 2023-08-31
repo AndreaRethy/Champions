8b7891bfd0e83aa84ba60615348ff9eb9ad45066
Andrea <125359387+AndreaRethy@users.noreply.github.com>
2023-08-31 17:26:17 +0200

    Add basic html

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // javascript
+// Strech goals:
+// Add From and To input fields
+// Reverse the order to show the last endorsment on the top
+// Add likes (but one user can only like once)
+// Make a desktop version
