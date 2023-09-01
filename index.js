455855ef08a20c2738e988090653c17be22dada3
Andrea <125359387+AndreaRethy@users.noreply.github.com>
2023-09-01 12:33:37 +0200

    set up javascript file with firebase database

--- index.js
+++ index.js
@@ -4,3 +4,15 @@
 // Reverse the order to show the last endorsment on the top
 // Add likes (but one user can only like once)
 // Make a desktop version
+
+import { initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+
+const appSettings = {
+    databaseURL: "https://playground-for-scrimba-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const endorsmentsInDB = ref(database, "endorsements")
